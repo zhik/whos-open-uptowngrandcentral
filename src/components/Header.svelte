279008ae16d4415f673fc4387241d13df5e5d9b4
Zhi Keng He <zhikhe@gmail.com>
2020-06-16 11:41:16 -0400

    Update header and add Google Analytics

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -32,7 +32,7 @@
         {/if}
     {:else}
         <div class="header-bar">
-            <h3 class="title is-5">Who's Open Queens</h3>
+            <h3 class="title is-5">Who's Open Uptown Grand Central</h3>
             <div class="buttons has-text-right">
                 <TranslationMenu className="is-small"/>
                 <button class="button is-small {opened ? 'is-clicked' : ''}" on:click={() => opened = !opened}>
@@ -42,14 +42,12 @@
         </div>
         {#if opened}
             <div id="subtitle">
-                <p class="is-6 subtitle"><strong>Community mapping of Queens' businesses operating
-                    during the COVID-19 crisis.</strong>
-                    <br/><br/>Changes are day to day. Please call ahead or check website/social media before you visit
+                <p class="is-6 subtitle"><strong>Businesses in Uptown Grand Central</strong>
+                    <br/><br/>Please call ahead or check website/social media before you visit
                     for the most current information.</p>
-                <p class="is-6 subtitle">In cooperation with Sunnyside Shines, Edible Queens, Queens Together, and
-                    Sunnyside Gardens Park.
-                    To add or update info fill out <a href="https://airtable.com/shrUCeoQpIRfNFh59" target="_blank">this
-                        form</a>.</p>
+                <p class="is-6 subtitle">Maintained by <a href="http://uptowngrandcentral.org" target="_blank">Uptown
+                    Grand Central</a>. To add or update info fill out <a href="https://airtable.com/shrbLNyrAUlnk45R7"
+                                                                         target="_blank">this form</a>.</p>
             </div>
         {/if}
     {/if}
